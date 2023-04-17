44774feea455413a2231c952af3bb3cdd11bab00
bandhavi <bandhavi.bendi@coupa.com>
2023-04-17 05:57:40 -0700

    added comments

--- tests/shape.test.js
+++ tests/shape.test.js
@@ -1,31 +1,45 @@
-const {Circle, Rectangle, Triangle} = require('../lib/shapes.js');
+const { Circle, Rectangle, Triangle } = require("../lib/shapes.js");
 
-describe('Shape tests',()=>{
 
-    it('should render the shape triange and set color to blue', () => {
+// Describing a test suite for the Triangle class
+describe("Shape tests", () => {
+  // Test case for rendering a blue triangle
+  test("should render the shape triange and set color to blue", () => {
     const shape = new Triangle();
     shape.setColor("blue");
-    expect(shape.render()).toEqual('<polygon points="0,200 300,200 150,0" fill="blue" />', 'Should render blue triangle polygon');
-    });
-})
+    expect(shape.render()).toEqual(
+      '<polygon points="0,200 300,200 150,0" fill="blue" />',
+      "Should render blue triangle polygon"
+    );
+  });
+});
 
-describe('shape color',()=>{
-
-    it('should render the shape circle and set color to red', () => {
+// Describing a test suite for the Circle class
+describe("shape color", () => {
+  // Test case for rendering a red circle
+  test("should render the shape circle and set color to red", () => {
     const shape = new Circle();
     shape.setShapeColor("red");
-    expect(shape.render()).toEqual('<circle height ="100%" width = "100%" cx="50%" cy="50%" r="100" stroke-width="3" fill="red" />', 'Should render red circle');
-    });
-})
+    // Asserting that the rendered circle matches the expected output
+
+    expect(shape.render()).toEqual(
+      '<circle height ="100%" width = "100%" cx="50%" cy="50%" r="100" stroke-width="3" fill="red" />',
+      "Should render red circle"
+    );
+  });
+});
 
 
-
-describe('rectangle',()=>{
-
-    it('should render the shape rectangle and set color to green', () => {
+// Describing a test suite for the Rectangle class
+describe("rectangle", () => {
+  // Test case for rendering a green rectangle
+  test("should render the shape rectangle and set color to green", () => {
     const shape = new Rectangle();
     shape.setColor("green");
-    expect(shape.render()).toEqual('<rect x="0%" y="30%" width="300" height="100" fill="green" />', 'Should render green rectangle');
-    });
-})
-
+    // Asserting that the rendered rectangle matches the expected output
+    expect(shape.render()).toEqual(
+      '<rect x="0%" y="30%" width="300" height="100" fill="green" />',
+      "Should render green rectangle"
+    );
+  });
+});
